f16088e642aff2e420c07c69f87994ee018cebcb
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-03-07 15:02:19 +0700

    1234

--- demo-pos/src/App.tsx
+++ demo-pos/src/App.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 import LoginPage from "./components/pages/LoginPage";
 import RegisterPage from "./components/pages/RegisterPage";
+import ReportPage from "./components/pages/ReportPage";
+import ShopPage from "./components/pages/ShopPage";
+import StockCreatePage from "./components/pages/StockCreatePage";
+import StockEditPage from "./components/pages/StockEditPage";
+import StockPage from "./components/pages/StockPage";
+import TransactionPage from "./components/pages/TransactionPage";
 import {
   BrowserRouter as Router,
   Redirect,
@@ -18,6 +24,7 @@ export default function App({}: Props) {
         <Switch>
           <Route path="/login" component={LoginPage} />
           <Route path="/register" component={RegisterPage} />
+          <Route path="/stock" component={StockPage} />
           <Route path="/notfound" component={PageNotFound} />
           <Redirect exact={true} path="/" to="/login" />
           <Redirect path="/" to="/notfound" />
