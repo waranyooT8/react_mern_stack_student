18b3edc7108ceac2b6a20fae80ac476dd68d7349
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-03-07 14:47:11 +0700

    1234

--- demo-pos/src/App.tsx
+++ demo-pos/src/App.tsx
@@ -1,12 +1,25 @@
 import React from "react";
 import LoginPage from "./components/pages/LoginPage";
+import RegisterPage from "./components/pages/RegisterPage";
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  RouteProps,
+  Switch,
+} from "react-router-dom";
 
 type Props = {};
 
 export default function App({}: Props) {
   return (
     <>
-      <LoginPage />
+      <Router>
+        <Switch>
+          <Route path="/login" component={LoginPage} />
+          <Route path="/register" component={RegisterPage} />
+        </Switch>
+      </Router>
     </>
   );
 }
