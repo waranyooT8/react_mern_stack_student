ade13997f98b2c9e9678d5a762f9a82dc6440e4c
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-03-07 11:04:53 +0700

    1234

--- demo0/src/App.js
+++ demo0/src/App.js
@@ -1,19 +1,15 @@
-import React from 'react'
+import React from "react";
 
-export default  function App() {
+export default function App() {
   return (
     <div>
-<div>App1</div>    
-<Header/>
+      <div>App1</div>
+      <Header />
     </div>
-    
-  )
+  );
 }
-
 
 // Create React Component
 function Header() {
-  return (
-    <h1>Header</h1>
-  )
+  return <h1>Header</h1>;
 }
