69da54f71fcafb2d8f8517fc3497f98d7ad512d4
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-03-08 13:03:29 +0700

    1234

--- demo-pos/backend/server.js
+++ demo-pos/backend/server.js
@@ -5,8 +5,8 @@ app.get("/", (req, res) => {
   res.json({ result: "ok" });
 });
 
-app.get("/login", (req, res) => {
-  res.json({ result: "login" });
+app.get("/test1/:username/:password", (req, res) => {
+  res.json({ result: "test1", echo: req.params });
 });
 
 app.listen(3000, () => console.log("Server is running"));
