93cf91e182502a81ecf464ad2e4c4b67a21dd6a9
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-03-08 10:51:34 +0700

    1234

--- demo-pos/src/components/pages/RegisterPage/RegisterPage.tsx
+++ demo-pos/src/components/pages/RegisterPage/RegisterPage.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { Formik } from "formik";
 
 type RegisterPageProps = {
   //
@@ -10,25 +11,36 @@ const RegisterPage: React.FC<any> = () => {
   return (
     <>
       <h1>RegisterPage</h1>
-      <form>
-        <label>Username: </label>
-        <input
-          type="text"
-          name="username"
-          onChange={(e) => setAccount({ ...account, username: e.target.value })}
-        />
-        <br />
-        <label>Password: </label>
-        <input
-          type="text"
-          name="password"
-          onChange={(e) => setAccount({ ...account, password: e.target.value })}
-        />{" "}
-        <br />
-        <button type="button" onClick={() => alert(JSON.stringify(account))}>
-          Submit
-        </button>
-      </form>
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        onSubmit={(values, { setSubmitting }) => {
+          
+        }}
+      >
+        <form>
+          <label>Username: </label>
+          <input
+            type="text"
+            name="username"
+            onChange={(e) =>
+              setAccount({ ...account, username: e.target.value })
+            }
+          />
+          <br />
+          <label>Password: </label>
+          <input
+            type="text"
+            name="password"
+            onChange={(e) =>
+              setAccount({ ...account, password: e.target.value })
+            }
+          />{" "}
+          <br />
+          <button type="button" onClick={() => alert(JSON.stringify(account))}>
+            Submit
+          </button>
+        </form>
+      </Formik>
     </>
   );
 };
