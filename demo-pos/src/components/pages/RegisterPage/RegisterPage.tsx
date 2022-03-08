a86d50a020d7b1a3950d780def5e06b7bcbb6698
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-03-08 10:55:08 +0700

    1234

--- demo-pos/src/components/pages/RegisterPage/RegisterPage.tsx
+++ demo-pos/src/components/pages/RegisterPage/RegisterPage.tsx
@@ -6,22 +6,28 @@ type RegisterPageProps = {
 };
 
 const RegisterPage: React.FC<any> = () => {
+  
+  const showForm = () => {
+    return (
+      <form>
+        <label>Username: </label>
+        <input type="text" name="username" />
+        <br />
+        <label>Password: </label>
+        <input type="text" name="password" /> <br />
+        <button type="submit">Submit</button>
+      </form>
+    );
+  };
+
   return (
     <>
       <h1>RegisterPage</h1>
       <Formik
         initialValues={{ username: "", password: "" }}
         onSubmit={(values, { setSubmitting }) => {}}
-      >
-        <form>
-          <label>Username: </label>
-          <input type="text" name="username" />
-          <br />
-          <label>Password: </label>
-          <input type="text" name="password" /> <br />
-          <button type="submit">Submit</button>
-        </form>
-      </Formik>
+        
+      ></Formik>
     </>
   );
 };
