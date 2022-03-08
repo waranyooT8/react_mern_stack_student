839fc660534bae7048c73c148da00cf755cb488a
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-03-08 13:03:57 +0700

    1234

--- demo-pos/backend/server.js
+++ demo-pos/backend/server.js
@@ -5,6 +5,7 @@ app.get("/", (req, res) => {
   res.json({ result: "ok" });
 });
 
+// http://localhost:3000/test1/lek/1234
 app.get("/test1/:username/:password", (req, res) => {
   res.json({ result: "test1", echo: req.params });
 });
