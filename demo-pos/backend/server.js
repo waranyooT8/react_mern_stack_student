4cd7608d6216441c09dc4f0c3e81c56a810eb495
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-03-08 13:57:22 +0700

    1234

--- demo-pos/backend/server.js
+++ demo-pos/backend/server.js
@@ -2,6 +2,6 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
-app.use("/api/v1", require("./api"));
+app.use("/api/v2", require("./api"));
 
-app.listen(3000, () => console.log("Server is running"));
+app.listen(8081, () => console.log("Server is running"));
