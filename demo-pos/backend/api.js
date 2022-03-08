fe94cce8015ed7c83c683ce56175dba1bf9b0d65
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-03-08 13:55:01 +0700

    1234

--- demo-pos/backend/api.js
+++ demo-pos/backend/api.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
+router.use(require("./api_auth"));
+router.use(require("./api_product"));
+router.use(require("./api_transaction"));
 
 module.exports = router;
