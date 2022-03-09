484ae6eca1b16fde6b1b5a53b7b50236ae29764a
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-03-09 11:43:30 +0700

    1234

--- demo-pos/backend/api_auth.js
+++ demo-pos/backend/api_auth.js
@@ -10,7 +10,7 @@ router.post("/login", async (req, res) => {
     const { username, password } = req.body;
     const doc = await Users.findOne({ username });
     if (doc && bcrypt.compareSync(password, doc.password)) {
-      const token = jwt.sign({ username });
+      const token = jwt.sign({ username }, 1000000);
 
       res.json({ result: "ok", token });
     } else {
