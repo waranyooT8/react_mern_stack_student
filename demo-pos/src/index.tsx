2c63c0371c63eb64eb678c4bf49724266fd4ac2a
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-03-09 15:11:39 +0700

    1234

--- demo-pos/src/index.tsx
+++ demo-pos/src/index.tsx
@@ -1,14 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
+import { createStore, applyMiddleware, Middleware } from "redux";
+import thunk from "redux-thunk";
+import { Provider } from "react-redux";
+import reducers from "./reducers";
+import logger from "redux-logger";
+
+const store = createStore(reducers, applyMiddleware(thunk));
+
+// HOC (Higher Order Component)
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
