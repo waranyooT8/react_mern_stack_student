f63df5d583ee6cbbb08d91b5d82c6eeac2309e4d
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-03-09 15:47:42 +0700

    1234

--- demo-pos/src/actions/register.action.ts
+++ demo-pos/src/actions/register.action.ts
@@ -1,1 +1,22 @@
-export {};
+import {
+  REGISTER_FAILED,
+  REGISTER_FETCHING,
+  REGISTER_SUCCESS,
+} from "../constants";
+import { RegisterResult } from "../types/auth-result.type";
+
+export const setRegisterFetchingToState = () => ({
+  type: REGISTER_FETCHING,
+});
+
+export const setRegisterSuccessToState = (payload: RegisterResult) => ({
+  type: REGISTER_SUCCESS,
+  payload,
+});
+
+export const setRegisterFailedToState = (payload: string) => ({
+  type: REGISTER_FAILED,
+  payload,
+});
+
+
