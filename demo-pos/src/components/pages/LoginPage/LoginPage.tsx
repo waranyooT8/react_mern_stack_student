9344a90f2ac9e33da8875081c31333aca58a1e4d
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-03-09 13:16:01 +0700

    1234

--- demo-pos/src/components/pages/LoginPage/LoginPage.tsx
+++ demo-pos/src/components/pages/LoginPage/LoginPage.tsx
@@ -1,19 +1,123 @@
-import * as React from "react";
-import { useHistory } from "react-router-dom";
+import { Box } from "@mui/material";
+import Alert from "@mui/material/Alert";
+import Button from "@mui/material/Button";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import CircularProgress from "@mui/material/CircularProgress";
+import Grid from "@mui/material/Grid";
+import Link from "@mui/material/Link";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+import { Formik } from "formik";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link as RouterLink, useHistory } from "react-router-dom";
+import { User } from "../../../types/user.type";
 
-type LoginPageProps = {
-  //
-};
+type LoginProps = {};
 
-const LoginPage: React.FC<any> = () => {
+export default (props: LoginProps) => {
   const history = useHistory();
 
+  const classes: any = {
+    root: { display: "flex", justifyContent: "center", alignItems: "center" },
+    submitBtn: { marginTop: 4 },
+    canelBtn: { marginTop: 2 },
+  };
+
+  useEffect(() => {
+    // loginActions.isLoggedIn() && props.history.push("/stock");
+  }, []);
+
+  const showForm = ({
+    values,
+    handleChange,
+    handleSubmit,
+    setFieldValue,
+    isSubmitting,
+  }: any) => {
+    return (
+      <form noValidate onSubmit={handleSubmit}>
+        <TextField
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          id="username"
+          label="Username"
+          onChange={handleChange}
+          value={values.username}
+          autoComplete="email"
+          autoFocus
+        />
+        <TextField
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          onChange={handleChange}
+          value={values.password}
+          name="password"
+          label="Password"
+          type="password"
+          id="password"
+          autoComplete="current-password"
+        />
+
+        <Button
+          sx={classes.submitBtn}
+          type="submit"
+          fullWidth
+          variant="contained"
+          color="primary"
+          disabled={isSubmitting}
+        >
+          Sign In
+        </Button>
+        {isSubmitting && <CircularProgress style={{ marginTop: 10 }} />}
+        <Grid container>
+          <Link component={RouterLink} to="/register" variant="body2">
+            Don't have an account? Register
+          </Link>
+        </Grid>
+
+        {/* Error Alert */}
+        {false && (
+          <Alert sx={{ marginTop: 2 }} severity="error">
+            There is something wrong!
+          </Alert>
+        )}
+      </form>
+    );
+  };
+
+  const initialValue: User = { username: "", password: "" };
+
   return (
-    <div>
-      <h1>LoginPage</h1>
-      <button onClick={() => history.push("/register")}>Register</button>
-    </div>
+    <Box sx={classes.root}>
+      <Card sx={{ maxWidth: 345 }}>
+        <CardMedia
+          component="img"
+          alt="Contemplative Reptile"
+          height="200"
+          image={`${process.env.PUBLIC_URL}/images/authen_header.jpg`}
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Login
+          </Typography>
+          <Formik
+            initialValues={initialValue}
+            onSubmit={(values, { setSubmitting }) => {
+              setSubmitting(false);
+            }}
+          >
+            {(props) => showForm(props)}
+          </Formik>
+        </CardContent>
+      </Card>
+    </Box>
   );
 };
-
-export default LoginPage;
