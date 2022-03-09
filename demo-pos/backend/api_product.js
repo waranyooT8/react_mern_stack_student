963cd0affda3c3f6ca67b148ecf7d21ccdf3e0f2
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-03-09 10:12:45 +0700

    1234

--- demo-pos/backend/api_product.js
+++ demo-pos/backend/api_product.js
@@ -2,6 +2,26 @@ const express = require("express");
 const router = express.Router();
 const Products = require("./models/product_schema");
 const formidable = require("formidable");
+const path = require("path");
+const fs = require("fs-extra");
+
+// Upload Image
+uploadImage = async (files, doc) => {
+  if (files.image != null) {
+    var fileExtention = files.image.originalFilename.split(".")[1];
+    doc.image = `${doc.product_id}.${fileExtention}`;
+    var newpath =
+      path.resolve(__dirname + "/uploaded/images/") + "/" + doc.image;
+
+    if (fs.existsSync(newpath)) {
+      await fs.remove(newpath);
+    }
+    await fs.move(files.image.filepath, newpath);
+
+    // Update database
+    await Products.findOneAndUpdate({ product_id: doc.product_id }, doc);
+  }
+};
 
 router.get("/product", async (req, res) => {
   const doc = await Products.find();
