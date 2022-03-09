1c371d03c072c64b53ba9deaf649135ea230cbda
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-03-09 15:52:21 +0700

    1234

--- demo-pos/src/actions/register.action.ts
+++ demo-pos/src/actions/register.action.ts
@@ -2,8 +2,11 @@ import {
   REGISTER_FAILED,
   REGISTER_FETCHING,
   REGISTER_SUCCESS,
+  server,
 } from "../constants";
 import { RegisterResult } from "../types/auth-result.type";
+import { User } from "../types/user.type";
+import { httpClient } from "../utils/HttpClient";
 
 export const setRegisterFetchingToState = () => ({
   type: REGISTER_FETCHING,
@@ -19,4 +22,21 @@ export const setRegisterFailedToState = (payload: string) => ({
   payload,
 });
 
-
+export const register = (user: User) => {
+  return async (dispatch: any) => {
+    try {
+      dispatch(setRegisterFetchingToState());
+      const result = await httpClient.post<RegisterResult>(
+        server.REGISTER_URL,
+        user
+      );
+      if (result.data.result === "ok") {
+        dispatch(setRegisterSuccessToState(result.data));
+      } else {
+        dispatch(setRegisterFailedToState("register error"));
+      }
+    } catch (e) {
+      dispatch(setRegisterFailedToState(JSON.stringify(e)));
+    }
+  };
+};
