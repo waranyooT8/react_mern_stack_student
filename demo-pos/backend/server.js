d3d3a61ba67f4b4c509b91d3256bbaa3622df0fb
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-03-09 13:09:22 +0700

    1234

--- demo-pos/backend/server.js
+++ demo-pos/backend/server.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const cors = require("cors")
+const cors = require("cors");
 const app = express();
 app.use(express.json());
-app.use(cors())
+app.use(cors());
+app.use(express.static(__dirname));
 
 app.use("/api/v2", require("./api"));
 
