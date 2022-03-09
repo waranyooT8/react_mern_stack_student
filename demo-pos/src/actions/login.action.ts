ec5ab6f4e53205923c31e8c8bf2a88bdfbd2b419
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-03-09 14:19:30 +0700

    1234

--- demo-pos/src/actions/login.action.ts
+++ demo-pos/src/actions/login.action.ts
@@ -1,5 +1,12 @@
-import { LOGIN_FAILED, LOGIN_FETCHING, LOGIN_SUCCESS } from "../constants";
+import {
+  LOGIN_FAILED,
+  LOGIN_FETCHING,
+  LOGIN_SUCCESS,
+  server,
+} from "../constants";
 import { LoginResult } from "../types/auth-result.type";
+import { User } from "../types/user.type";
+import { httpClient } from "../utils/HttpClient";
 
 export const setLoginFetchingToState = () => ({
   type: LOGIN_FETCHING,
@@ -10,7 +17,16 @@ export const setLoginSuccessToState = (payload: LoginResult) => ({
   payload,
 });
 
-export const setLoginFailedToState = (payload: LoginResult) => ({
+export const setLoginFailedToState = (payload: string) => ({
   type: LOGIN_FAILED,
   payload,
 });
+
+export const login = (user: User) => {
+  try {
+    setLoginFetchingToState();
+    const result = await httpClient.post<LoginResult>(server.LOGIN_URL, user);
+  } catch (error) {
+    setLoginFailedToState(JSON.stringify(error));
+  }
+};
