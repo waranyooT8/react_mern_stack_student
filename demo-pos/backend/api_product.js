53eb113c2ce574c7ff1624d068128cb10eb85d0c
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-03-09 10:06:48 +0700

    1234

--- demo-pos/backend/api_product.js
+++ demo-pos/backend/api_product.js
@@ -1,10 +1,23 @@
 const express = require("express");
 const router = express.Router();
 const Products = require("./models/product_schema");
+const formidable = require("formidable");
 
 router.get("/product", async (req, res) => {
   const doc = await Products.find();
   res.json(doc);
+});
+
+// Add Product
+router.post("/product", async (req, res) => {
+  try {
+    const form = new formidable.IncomingForm();
+    form.parse(req, (err, fields, files) => {
+      res.json({ result: "ok", err, fields, files });
+    });
+  } catch (err) {
+    res.json({ result: "nok", message: JSON.stringify(err) });
+  }
 });
 
 module.exports = router;
