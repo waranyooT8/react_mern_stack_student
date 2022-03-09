7d3338d3e7cbebfdb8d02bb1ddf8d2e49b791c8f
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-03-09 13:43:08 +0700

    1234

--- demo-pos/src/types/auth-result.type.ts
+++ demo-pos/src/types/auth-result.type.ts
@@ -1,5 +1,11 @@
 export interface LoginResult {
   result: string;
   token: string;
+  refreshToken: string;
+  error?: string;
+}
+
+export interface RegisterResult {
+  result: string;
   error?: string;
 }
