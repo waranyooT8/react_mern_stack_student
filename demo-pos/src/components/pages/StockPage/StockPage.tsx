0402be6d2aa1cedc432cd3370fbf818c8a462b1e
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-03-10 10:53:17 +0700

    1234

--- demo-pos/src/components/pages/StockPage/StockPage.tsx
+++ demo-pos/src/components/pages/StockPage/StockPage.tsx
@@ -45,15 +45,11 @@ const StockPage: React.FC<any> = () => {
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            <TableCell>Name</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
+          {stockReducer.result.map((row) => (
             <TableRow
               key={row.name}
               sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
@@ -61,10 +57,6 @@ const StockPage: React.FC<any> = () => {
               <TableCell component="th" scope="row">
                 {row.name}
               </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
             </TableRow>
           ))}
         </TableBody>
