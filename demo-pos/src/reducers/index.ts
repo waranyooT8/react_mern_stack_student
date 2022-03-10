47b050cc500035f5a6e74201a5bb8904128030ba
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-03-10 09:48:16 +0700

    1234

--- demo-pos/src/reducers/index.ts
+++ demo-pos/src/reducers/index.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import loginReducer, { LoginState } from "./login.reducer";
 import registerReducer, { RegisterState } from "./register.reducer";
+import stockReducer, { StockState } from "./stock.reducer";
 
-export default combineReducers({ loginReducer, registerReducer });
+export default combineReducers({ loginReducer, registerReducer, stockReducer });
 
 export interface RootReducer {
   loginReducer: LoginState;
   registerReducer: RegisterState;
+  stockReducer: StockState;
 }
