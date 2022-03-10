25cbd44ab02c582e8b221cbe469c8790a44514b7
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-03-10 15:32:42 +0700

    1234

--- demo-pos/src/components/pages/ReportPage/ReportPage.tsx
+++ demo-pos/src/components/pages/ReportPage/ReportPage.tsx
@@ -1,40 +1,141 @@
-import * as React from "react";
+import RefreshIcon from "@mui/icons-material/Refresh";
+import { Button, ButtonGroup, IconButton, Paper } from "@mui/material";
+import Typography from "@mui/material/Typography";
+import React, { useEffect, useState } from "react";
 import "chart.js/auto";
 import { Chart } from "react-chartjs-2";
 
-type ReportPageProps = {
-  //
-};
+export default () => {
+  const [chartType, setChartType] = useState<string>("bar");
+  const [chartData, setChartData] = useState([]);
 
-const data: any = {
-  labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-  datasets: [
-    {
-      label: "# of Votes",
-      data: [12, 19, 3, 5, 2, 3],
-      backgroundColor: [
-        "rgba(255, 99, 132, 0.2)",
-        "rgba(54, 162, 235, 0.2)",
-        "rgba(255, 206, 86, 0.2)",
-        "rgba(75, 192, 192, 0.2)",
-        "rgba(153, 102, 255, 0.2)",
-        "rgba(255, 159, 64, 0.2)",
+  function getRandomInt(): any {
+    let randoms = [];
+    for (let index = 0; index < 8; index++) {
+      randoms.push(Math.floor(Math.random() * (50000 - 5 + 1)) + 5);
+    }
+    return randoms;
+  }
+
+  const data: any = {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [
+      {
+        label: "Revenue 2022",
+        fill: true,
+        lineTension: 0.1,
+        backgroundColor: [
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+          "rgba(255, 99, 132, 0.2)",
+        ],
+        borderColor: "rgba(75,192,192,1)",
+        borderCapStyle: "butt",
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: "miter",
+        pointBorderColor: "rgba(75,192,192,1)",
+        pointBackgroundColor: "#fff",
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: "rgba(75,192,192,1)",
+        pointHoverBorderColor: "rgba(220,220,220,1)",
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: chartData,
+      },
+    ],
+  };
+
+  const chartOption: any = {
+    maintainAspectRatio: false,
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true,
+            callback: function (value: any, index: any, values: any) {
+              return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+            },
+          },
+        },
       ],
-      borderColor: [
-        "rgba(255, 99, 132, 1)",
-        "rgba(54, 162, 235, 1)",
-        "rgba(255, 206, 86, 1)",
-        "rgba(75, 192, 192, 1)",
-        "rgba(153, 102, 255, 1)",
-        "rgba(255, 159, 64, 1)",
-      ],
-      borderWidth: 1,
     },
-  ],
-};
+  };
 
-const ReportPage: React.FC<any> = () => {
-  return <Chart type="line" data={data} />;
-};
+  useEffect(() => {
+    setChartData(getRandomInt());
+  }, []);
 
-export default ReportPage;
+  return (
+    <Paper sx={{ padding: 4 }}>
+      <Typography variant="h1">Sale Chart</Typography>
+      <ButtonGroup
+        size="large"
+        color="primary"
+        aria-label="large outlined primary button group"
+      >
+        <Button
+          variant={chartType === "line" ? "contained" : "outlined"}
+          onClick={() => setChartType("line")}
+        >
+          Line
+        </Button>
+        <Button
+          variant={chartType === "bar" ? "contained" : "outlined"}
+          onClick={() => setChartType("bar")}
+        >
+          Bar
+        </Button>
+        <Button
+          variant={chartType === "pie" ? "contained" : "outlined"}
+          onClick={() => setChartType("pie")}
+        >
+          Pie
+        </Button>
+      </ButtonGroup>
+      <IconButton
+        aria-label="refresh"
+        onClick={() => {
+          setChartData(getRandomInt());
+        }}
+      >
+        <RefreshIcon />
+      </IconButton>
+      <div style={{ height: 500 }}>
+        {chartType === "line" && (
+          <Chart
+            type="line"
+            data={data}
+            width="100%"
+            height={50}
+            options={chartOption}
+          />
+        )}
+        {chartType === "pie" && (
+          <Chart
+            type="pie"
+            data={data}
+            width="100%"
+            height={50}
+            options={chartOption}
+          />
+        )}
+        {chartType === "bar" && (
+          <Chart
+            type="bar"
+            data={data}
+            width="100%"
+            height={50}
+            options={chartOption}
+          />
+        )}
+      </div>
+    </Paper>
+  );
+};
