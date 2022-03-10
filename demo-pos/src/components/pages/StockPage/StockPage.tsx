8fb797654298a17bc1549b4ea3d53abb6be9ab86
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-03-10 11:09:02 +0700

    1234

--- demo-pos/src/components/pages/StockPage/StockPage.tsx
+++ demo-pos/src/components/pages/StockPage/StockPage.tsx
@@ -1,97 +1,264 @@
-import * as React from "react";
-import { useDispatch, useSelector } from "react-redux";
-import * as stockActions from "../../../actions/stock.action";
-import { RootReducer } from "../../../reducers";
+import AddIcon from "@mui/icons-material/Add";
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+import AssignmentReturnIcon from "@mui/icons-material/AssignmentReturn";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
+import NewReleasesIcon from "@mui/icons-material/NewReleases";
+import SearchIcon from "@mui/icons-material/Search";
+import StarIcon from "@mui/icons-material/Star";
+import {
+  Box,
+  Divider,
+  Grid,
+  IconButton,
+  InputBase,
+  Typography,
+} from "@mui/material";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import Paper from "@mui/material/Paper";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
-import { imageUrl } from "../../../constants";
+import { useEffect, useState } from "react";
 import Moment from "react-moment";
 import NumberFormat from "react-number-format";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { RootReducer } from "../../../reducers";
+import * as stockActions from "./../../../actions/stock.action";
+import { imageUrl } from "./../../../constants";
+// import StockCard from "./../../fragments/StockCard/StockCard";
+import { useDebounce, useDebounceCallback } from "@react-hook/debounce";
 
-type StockPageProps = {
-  //
-};
-
-const StockPage: React.FC<any> = () => {
-  const dispatch = useDispatch();
+export default (props: any) => {
   const stockReducer = useSelector((state: RootReducer) => state.stockReducer);
+  const dispatch = useDispatch();
 
-  React.useEffect(() => {
-    dispatch(stockActions.loadStock());
+  const [openDialog, setOpenDialog] = useState<boolean>(false);
+  const [selectedProduct, setSelectedProduct] = useState<any>(null);
+  const [value, setValue] = useDebounce("", 500);
+
+  useEffect(() => {
+    // dispatch(stockActions.getProductByKeyword(value));
+  }, [value]);
+
+  useEffect(() => {
+    // dispatch(stockActions.getProducts());
   }, []);
 
-  function createData(
-    name: string,
-    calories: number,
-    fat: number,
-    carbs: number,
-    protein: number
-  ) {
-    return { name, calories, fat, carbs, protein };
-  }
-
-  const rows = [
-    createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-    createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-    createData("Eclair", 262, 16.0, 24, 6.0),
-    createData("Cupcake", 305, 3.7, 67, 4.3),
-    createData("Gingerbread", 356, 16.0, 49, 3.9),
+  const actions = [
+    {
+      icon: "edit",
+      iconProps: { color: "primary" },
+      tooltip: "Edit",
+      onClick: (event: any, rowData: any) =>
+        props.history.push("/stock/edit/" + rowData.product_id),
+    },
+    {
+      icon: "delete",
+      iconProps: { color: "action" },
+      tooltip: "Delete",
+      onClick: (event: any, rowData: any) => {
+        setSelectedProduct(rowData);
+        setOpenDialog(true);
+      },
+    },
   ];
 
+  const handleDeleteConfirm = () => {
+    // dispatch(stockActions.deleteProduct(selectedProduct.product_id));
+    // dispatch(stockActions.getProducts());
+    setOpenDialog(false);
+  };
+
+  const handleClose = () => {
+    setOpenDialog(false);
+  };
+
+  const showDialog = () => {
+    if (selectedProduct === null) {
+      return "";
+    }
+
+    return (
+      <Dialog
+        open={openDialog}
+        keepMounted
+        onClose={() => {}}
+        aria-labelledby="alert-dialog-slide-title"
+        aria-describedby="alert-dialog-slide-description"
+      >
+        <DialogTitle id="alert-dialog-slide-title">
+          <img
+            src={`${imageUrl}/images/${
+              selectedProduct.image
+            }?dummy=${Math.random()}`}
+            style={{ width: 100, borderRadius: "5%" }}
+          />
+          <br />
+          Confirm to delete the product? : {" " + selectedProduct.name}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-slide-description">
+            You cannot restore deleted product.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="info">
+            Cancel
+          </Button>
+          <Button onClick={handleDeleteConfirm} color="primary">
+            Delete
+          </Button>
+        </DialogActions>
+      </Dialog>
+    );
+  };
+
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Image</TableCell>
-            <TableCell>Name</TableCell>
-            <TableCell>Stock</TableCell>
-            <TableCell>Price</TableCell>
-            <TableCell>Created</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {stockReducer.result.map((row) => (
-            <TableRow
-              key={row.name}
-              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                <img
-                  src={`${imageUrl}/images/${row.image}?dummy=${Math.random()}`}
-                  style={{ width: 70, height: 70, borderRadius: "5%" }}
-                />
-              </TableCell>
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell component="th" scope="row">
-                {row.stock}
-              </TableCell>
-              <TableCell component="th" scope="row">
-                <NumberFormat
-                  value={row.price}
-                  displayType={"text"}
-                  thousandSeparator={true}
-                  decimalScale={0}
-                  suffix={"THB"}
-                  fixedDecimalScale={true}
-                />
-              </TableCell>
-              <TableCell component="th" scope="row">
-                <Moment format="DD/MM/YYYY HH:mm">{row.created}</Moment>
-              </TableCell>
+    <Box>
+      {/* Summary Icons */}
+
+      <Paper
+        component="form"
+        sx={{
+          p: "2px 4px",
+          marginTop: 10,
+          display: "flex",
+          alignItems: "center",
+          width: 400,
+          marginBottom: 2,
+        }}
+      >
+        <input type="text" hidden />
+        <InputBase
+          onChange={(e) => {
+            setValue(e.target.value);
+            e.stopPropagation();
+            e.preventDefault();
+          }}
+          sx={{ ml: 1, flex: 1 }}
+          placeholder="Search product name"
+          inputProps={{ "aria-label": "search product name" }}
+        />
+        <SearchIcon />
+        <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+
+        <IconButton
+          type="button"
+          sx={{ p: "10px" }}
+          aria-label="add"
+          component={Link}
+          to="/stock/create"
+        >
+          <AddIcon />
+        </IconButton>
+      </Paper>
+
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} size="small" aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Id</TableCell>
+              <TableCell align="left">Image</TableCell>
+              <TableCell align="left">Name</TableCell>
+              <TableCell align="right">Price</TableCell>
+              <TableCell align="right">Stock</TableCell>
+              <TableCell align="right">Created</TableCell>
+              <TableCell align="left">Actions</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {stockReducer.result.map((item: any) => (
+              <TableRow hover key={item.product_id} sx={{ padding: 0 }}>
+                {/* Id */}
+                <TableCell component="th" scope="row">
+                  <Typography variant="body1">{item.product_id}</Typography>
+                </TableCell>
+
+                {/* Image */}
+                <TableCell align="left">
+                  <img
+                    src={`${imageUrl}/images/${
+                      item.image
+                    }?dummy=${Math.random()}`}
+                    style={{ width: 70, height: 70, borderRadius: "5%" }}
+                  />
+                </TableCell>
+                <TableCell align="left">
+                  <Typography variant="body1">{item.name}</Typography>
+                </TableCell>
+                {/* Price */}
+                <TableCell align="right">
+                  <Typography variant="body1">
+                    <NumberFormat
+                      value={item.price}
+                      displayType={"text"}
+                      thousandSeparator={true}
+                      decimalScale={2}
+                      fixedDecimalScale={true}
+                      prefix={"฿"}
+                    />
+                  </Typography>
+                </TableCell>
+                {/* Stock */}
+                <TableCell align="right">
+                  <Typography variant="body1">
+                    <NumberFormat
+                      value={item.stock}
+                      displayType={"text"}
+                      thousandSeparator={true}
+                      decimalScale={0}
+                      fixedDecimalScale={true}
+                    />
+                  </Typography>
+                </TableCell>
+                {/* Create */}
+                <TableCell align="right">
+                  <Typography>
+                    <Moment format="DD/MM/YYYY">{item.created}</Moment>
+                  </Typography>
+                </TableCell>
+
+                {/* Actions */}
+                <TableCell align="left">
+                  <Box sx={{ display: "flex", flexDirection: "row" }}>
+                    <IconButton
+                      aria-label="edit"
+                      size="large"
+                      onClick={() => {
+                        props.history.push("/stock/edit/" + item.product_id);
+                      }}
+                    >
+                      <EditIcon fontSize="inherit" />
+                    </IconButton>
+                    <IconButton
+                      aria-label="delete"
+                      size="large"
+                      onClick={() => {
+                        setSelectedProduct(item);
+                        setOpenDialog(true);
+                      }}
+                    >
+                      <DeleteIcon fontSize="inherit" />
+                    </IconButton>
+                  </Box>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+
+      {showDialog()}
+    </Box>
   );
 };
-
-export default StockPage;
