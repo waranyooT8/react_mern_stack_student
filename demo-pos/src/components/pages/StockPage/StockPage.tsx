c90b6a05af49ae16d9bdd51b2ba284d77a977889
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-03-10 09:55:10 +0700

    1234

--- demo-pos/src/components/pages/StockPage/StockPage.tsx
+++ demo-pos/src/components/pages/StockPage/StockPage.tsx
@@ -1,10 +1,20 @@
 import * as React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import * as stockActions from "../../../actions/stock.action";
+import { RootReducer } from "../../../reducers";
 
 type StockPageProps = {
   //
 };
 
 const StockPage: React.FC<any> = () => {
+  const dispatch = useDispatch();
+  const stockReducer = useSelector((state: RootReducer) => state.stockReducer);
+
+  React.useEffect(() => {
+    dispatch(stockActions.loadStock());
+  }, []);
+
   return (
     <>
       <h1>StockPage</h1>
