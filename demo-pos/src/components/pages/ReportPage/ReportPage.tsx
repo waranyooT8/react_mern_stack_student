53fbaf9061d2ead07de116232b6315f323d73a3c
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-03-10 15:40:23 +0700

    1234

--- demo-pos/src/components/pages/ReportPage/ReportPage.tsx
+++ demo-pos/src/components/pages/ReportPage/ReportPage.tsx
@@ -49,6 +49,35 @@ export default () => {
         pointHitRadius: 10,
         data: chartData,
       },
+      {
+        label: "Revenue 2023",
+        fill: true,
+        lineTension: 0.1,
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+          "rgba(255, 99, 132, 0.2)",
+        ],
+        borderColor: "rgba(75,192,192,1)",
+        borderCapStyle: "butt",
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: "miter",
+        pointBorderColor: "rgba(75,192,192,1)",
+        pointBackgroundColor: "#fff",
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: "rgba(75,192,192,1)",
+        pointHoverBorderColor: "rgba(220,220,220,1)",
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: chartData,
+      },
     ],
   };
 
@@ -110,7 +139,7 @@ export default () => {
       <div style={{ height: 500 }}>
         {chartType === "line" && (
           <Chart
-            type="line"
+            type={"line"}
             data={data}
             width="100%"
             height={50}
