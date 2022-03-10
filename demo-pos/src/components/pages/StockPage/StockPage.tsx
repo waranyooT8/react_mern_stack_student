302c7b67ab386ae0eac8187c6cbaa99131c10e31
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-03-10 11:40:37 +0700

    1234

--- demo-pos/src/components/pages/StockPage/StockPage.tsx
+++ demo-pos/src/components/pages/StockPage/StockPage.tsx
@@ -64,6 +64,33 @@ export default (props: any) => {
     setOpenDialog(false);
   };
 
+  const showMyDialog = () => {
+    return (
+      <Dialog
+        open={openMy}
+        onClose={handleCloseMy}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {"Use Google's location service?"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Let Google help apps determine location. This means sending
+            anonymous location data to Google, even when no apps are running.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseMy}>Disagree</Button>
+          <Button onClick={handleCloseMy} autoFocus>
+            Agree
+          </Button>
+        </DialogActions>
+      </Dialog>
+    );
+  };
+
   const showDialog = () => {
     if (selectedProduct === null) {
       return "";
