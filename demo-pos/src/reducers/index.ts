79ba9e7cf399e2bccd412e248ec0ab3ea8d475d8
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-03-10 13:43:55 +0700

    1234

--- demo-pos/src/reducers/index.ts
+++ demo-pos/src/reducers/index.ts
@@ -2,11 +2,17 @@ import { combineReducers } from "redux";
 import loginReducer, { LoginState } from "./login.reducer";
 import registerReducer, { RegisterState } from "./register.reducer";
 import stockReducer, { StockState } from "./stock.reducer";
-
-export default combineReducers({ loginReducer, registerReducer, stockReducer });
+import stockEditReducer, { StockEditState } from "./stock.edit.reducer";
+export default combineReducers({
+  loginReducer,
+  registerReducer,
+  stockReducer,
+  stockEditReducer,
+});
 
 export interface RootReducer {
   loginReducer: LoginState;
   registerReducer: RegisterState;
   stockReducer: StockState;
+  stockEditReducer: StockEditState;
 }
