8d89cbb022c61ba112509d01e17dc147b9861873
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-03-11 09:17:01 +0700

    1234

--- demo-pos/backend/api_transaction.js
+++ demo-pos/backend/api_transaction.js
@@ -3,7 +3,17 @@ const router = express.Router();
 const Transactions = require("./models/trans_schema");
 
 router.get("/transaction", async (req, res) => {
-  const doc = await Transactions.aggregate([{ $sort: { timestamp: -1 } }]);
+  const doc = await Transactions.aggregate([
+    { $sort: { timestamp: -1 } },
+    {
+      $lookup: {
+        from: "users",
+        localField: "staff_id",
+        foreignField: "_id",
+        as: "staff",
+      },
+    },
+  ]);
   res.json(doc);
 });
 
